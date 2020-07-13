00e6e83abf1101b0e1611727511ec6c16f43a70c
Veselinbg <vesko.i@gmail.com>
2020-07-14 01:37:06 +0300

    Remove unused using

--- Bizportal.Api.NUnit/BizportalUnitTest.cs
+++ Bizportal.Api.NUnit/BizportalUnitTest.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
